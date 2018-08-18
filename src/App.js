e48b6564eb0568bf5c9d73ba2a62d9081ab0950e
William Kwok <wkwok16@uw.edu>
2018-08-18 16:48:42 -0700

    fdsa

--- src/App.js
+++ src/App.js
@@ -1,22 +1,62 @@
 import React, { Component } from 'react';
 import { PDFExport } from '@progress/kendo-react-pdf';
+import { faGithub, faMedium } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import canvg from 'canvg';
+import ReactDOMServer from 'react-dom/server';
 
 class App extends Component {
     resume;
 
     constructor() {
         super();
+        this.iconsToConvert = [
+            {
+                icon: faGithub,
+                alt: 'github icon'
+            },
+            {
+                icon: faMedium,
+                alt: 'medium icon'
+            }
+        ]
+        this.canvLoaded = false;
     }
 
     exportPDF = () => {
         this.resume.save();
     }
 
+    convertSvgToImage = (arr) => {
+        let canv = this.refs.canvas;
+
+        if (canv) {
+            canv.getContext("2d");
+            arr.forEach((d, i) => {
+                let htmlString = ReactDOMServer.renderToStaticMarkup(
+                    <FontAwesomeIcon icon={d.icon} size={"3x"} style={{ color: '#005696', height: '500px', width: '500px' }} />
+                );
+                canvg(canv, htmlString);
+                d.icon = canv.toDataURL("image/png");
+            })
+        }
+
+    }
+
     render() {
+
+        if (!this.canvLoaded) {
+            console.log('a');
+            this.canvLoaded = true;
+
+            this.convertSvgToImage(this.iconsToConvert);
+        }
+
         return (
             <div style={{ height: '100vh', width: '100vw', paddingTop: 20, backgroundColor: 'gray' }}>
+                <canvas ref="canvas" style={{ display: 'none' }}>
+                </canvas>
                 <div style={{ textAlign: 'center', marginBottom: 10 }}><button onClick={this.exportPDF} style={{ margin: 'auto' }}>download</button></div>
-
                 <PDFExport paperSize={'Letter'}
                     fileName="_____.pdf"
                     title=""
@@ -32,7 +72,11 @@ class App extends Component {
                         margin: 'auto',
                         overflowX: 'hidden',
                         overflowY: 'hidden'
-                    }}>content</div>
+                    }}>Hi!
+                    {this.canvLoaded && this.iconsToConvert.map((iconObject, index) => {
+                            return <img src={iconObject.icon} key={'img-' + index} alt={iconObject.alt} styles={{ height: 25, width: 25 }} />
+                        })}
+                    </div>
                 </PDFExport>
             </div>
         );
